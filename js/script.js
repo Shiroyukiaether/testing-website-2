de4bad5a521269ee07c21b00574cbd1206f93145
Fathi XI B <shiroyukiaether@gnmail.com>
2024-01-15 06:11:39 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector ('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    e.preventDefault();
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+   searchForm.classList.toggle('active');
+   searchBox.focus();
+   e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector ('#hamburger-menu');
+const sb = document.querySelector ('#search-button');
+
+document.addEventListener('click', function (e) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+});
+
+let circle = document.querySelector('.circle');
+let slider = document.querySelector('.slider');
+let list = document.querySelector('.list');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let items = document.querySelectorAll('.list .item');
+let count = items.length;
+let active = 1;
+let leftTransform = 0;
+let width_item = items[active].offsetWidth;
+
+next.onclick = () => {
+    active = active >= count - 1 ? count - 1 : active + 1;
+    runCarousel();
+}
+prev.onclick = () => {
+    active = active <= 0 ? active : active - 1;
+    runCarousel();
+}
+function runCarousel() {
+    prev.style.display = (active == 0) ? 'none' : 'block';
+    next.style.display = (active == count - 1) ? 'none' : 'block';
+
+
+    let old_active = document.querySelector('.item.active');
+    if(old_active) old_active.classList.remove('active');
+    items[active].classList.add('active');
+
+     leftTransform = width_item * (active - 1) * -1;
+    list.style.transform = `translateX(${leftTransform}px)`;
+}
+runCarousel();
+
+
+// Set Text on a Circle
+let textCircle = circle.innerText.split('');
+circle.innerText = '';
+textCircle.forEach((value, key) => {
+    let newSpan =  document.createElement("span");
+    newSpan.innerText = value;
+    let rotateThisSpan = (360 / textCircle.length) * (key + 1);
+    newSpan.style.setProperty('--rotate', rotateThisSpan + 'deg');
+    circle.appendChild(newSpan); 
+});
